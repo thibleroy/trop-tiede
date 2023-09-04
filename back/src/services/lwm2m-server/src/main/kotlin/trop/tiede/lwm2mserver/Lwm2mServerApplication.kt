2a62473cfba5c8d6dd4319c85125f64cc404fe2d
thibleroy <thibleroy23@gmail.com>
2023-09-04 23:47:09 +0200

    feat(record): try

--- back/src/services/lwm2m-server/src/main/kotlin/trop/tiede/lwm2mserver/Lwm2mServerApplication.kt
+++ back/src/services/lwm2m-server/src/main/kotlin/trop/tiede/lwm2mserver/Lwm2mServerApplication.kt
@@ -1,105 +1,196 @@
 package trop.tiede.lwm2mserver
 
-import org.eclipse.californium.scandium.config.DtlsConnectorConfig
-import org.eclipse.leshan.core.californium.PrincipalMdcConnectionListener
+import com.fasterxml.jackson.core.JsonProcessingException
+import org.eclipse.leshan.core.node.TimestampedLwM2mNodes
 import org.eclipse.leshan.core.observation.CompositeObservation
 import org.eclipse.leshan.core.observation.Observation
 import org.eclipse.leshan.core.observation.SingleObservation
+import org.eclipse.leshan.core.request.ReadRequest
+import org.eclipse.leshan.core.request.SendRequest
 import org.eclipse.leshan.core.response.ObserveCompositeResponse
 import org.eclipse.leshan.core.response.ObserveResponse
+import org.eclipse.leshan.core.response.ReadResponse
 import org.eclipse.leshan.server.LeshanServer
 import org.eclipse.leshan.server.LeshanServerBuilder
 import org.eclipse.leshan.server.californium.endpoint.CaliforniumServerEndpointsProvider
 import org.eclipse.leshan.server.californium.endpoint.coap.CoapServerProtocolProvider
-import org.eclipse.leshan.server.californium.endpoint.coaps.CoapsServerProtocolProvider
 import org.eclipse.leshan.server.observation.ObservationListener
+import org.eclipse.leshan.server.queue.PresenceListener
 import org.eclipse.leshan.server.registration.Registration
 import org.eclipse.leshan.server.registration.RegistrationListener
 import org.eclipse.leshan.server.registration.RegistrationUpdate
+import org.eclipse.leshan.server.send.SendListener
 import org.springframework.boot.CommandLineRunner
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
-import java.lang.Exception
-import java.util.*
-
+import kotlin.system.exitProcess
 
 @SpringBootApplication
 class Lwm2mServerApplication {
-	lateinit var server: LeshanServer
+    lateinit var server: LeshanServer
 
-	fun main(args: Array<String>) {
-		SpringApplication.run(Lwm2mServerApplication::class.java, *args)
-	}
+    @Bean
+    fun commandLineRunner(ctx: ApplicationContext): CommandLineRunner? {
+        return CommandLineRunner {
+            try {
+                this.init()
+                this.server.start()
+            } catch (e: Exception) {
+                print("Error on init/start LwM2M server. Error is: ${e.message}")
+                exitProcess(1)
+            }
+        }
+    }
 
-	@Bean
-	fun commandLineRunner(ctx: ApplicationContext): CommandLineRunner? {
-		return CommandLineRunner {
-			this.init()
-			this.server.start()
-		}
-	}
-	fun init() {
-		val builder = LeshanServerBuilder()
-		val endpointsBuilder = CaliforniumServerEndpointsProvider.Builder( // Add coap Protocol support
-			CoapServerProtocolProvider())  // Add coaps protocol support
-		builder.setEndpointsProvider(endpointsBuilder.build())
-		this.server = builder.build()
-		this.server.observationService.addListener(object : ObservationListener {
-			override fun newObservation(observation: Observation?, registration: Registration?) {
-				println("new obs $observation.id")
-			}
+    fun init() {
+        val builder = LeshanServerBuilder()
+        val endpointsBuilder = CaliforniumServerEndpointsProvider.Builder( // Add coap Protocol support
+            CoapServerProtocolProvider()
+        )  // Add COAPs protocol support
+        builder.setEndpointsProvider(endpointsBuilder.build())
+        this.server = builder.build()
+        this.initServerObservationService()
+        this.initServerRegistrationService()
+        this.initServerSendService()
+        this.initServerPresenceService()
+        // Enregistrer un observateur pour les observations
+    }
 
-			override fun cancelled(observation: Observation?) {
-				TODO("Not yet implemented")
-			}
+    fun initServerPresenceService() {
+        this.server.presenceService.addListener(object : PresenceListener {
+            override fun onAwake(registration: Registration?) {
+                println("device is awake ${registration.toString()}")
+            }
 
-			override fun onResponse(
-				observation: SingleObservation?,
-				registration: Registration?,
-				response: ObserveResponse?
-			) {
-				TODO("Not yet implemented")
-			}
+            override fun onSleeping(registration: Registration?) {
+                println("device is sleeping ${registration.toString()}")
+            }
+        })
+    }
 
-			override fun onResponse(
-				observation: CompositeObservation?,
-				registration: Registration?,
-				response: ObserveCompositeResponse?
-			) {
-				TODO("Not yet implemented")
-			}
+    fun initServerSendService() {
+        this.server.sendService.addListener(object : SendListener {
+            override fun dataReceived(
+                registration: Registration?,
+                data: TimestampedLwM2mNodes?,
+                request: SendRequest?
+            ) {
+                if (registration != null) {
+                    try {
+                        for (node in data!!.nodes) {
+                            println("*** DATA RECEIVED *** /")
+                            when (node.value.id) {
+                                5700 -> print("Sensor Value ${node.value}")
+                                else -> { // Note the block
+                                    print("data node value id not handled")
+                                }
+                            }
+                            println("node ${node.value}")
+                        }
+                    } catch (e: JsonProcessingException) {
+                        println("Error while processing json [${data.toString()}] : [${e.message}]")
+                    }
+                }
 
-			override fun onError(observation: Observation?, registration: Registration?, error: Exception?) {
-				TODO("Not yet implemented")
-			}
+            }
 
-		})
-		this.server.registrationService.addListener(object : RegistrationListener {
-			override fun registered(
-				registration: Registration, previousReg: Registration?,
-				previousObsersations: Collection<Observation?>?
-			) {
-				println("new device: " + registration.endpoint)
-			}
+            override fun onError(registration: Registration?, error: java.lang.Exception?) {
+                if (error != null) {
+                    print("sendService error is ${error.message}")
+                    return
+                }
+                print("sendService registration is ${registration.toString()}")
+            }
 
-			override fun updated(update: RegistrationUpdate?, updatedReg: Registration, previousReg: Registration?) {
-				println("device is still here: " + updatedReg.endpoint)
-			}
 
-			override fun unregistered(
-				registration: Registration, observations: Collection<Observation?>?, expired: Boolean,
-				newReg: Registration?
-			) {
-				println("device left: " + registration.endpoint)
-			}
-		})
+        })
+    }
 
-	}
+    fun initServerObservationService() {
+        this.server.observationService.addListener(object : ObservationListener {
+            override fun newObservation(observation: Observation?, registration: Registration?) {
+                println("new obs $observation.id")
+            }
+
+            override fun cancelled(observation: Observation?) {
+                println("new obs $observation.id")
+            }
+
+            override fun onResponse(
+                observation: SingleObservation?,
+                registration: Registration?,
+                response: ObserveResponse?
+            ) {
+                println("new obs $observation.id")
+            }
+
+            override fun onResponse(
+                observation: CompositeObservation?,
+                registration: Registration?,
+                response: ObserveCompositeResponse?
+            ) {
+                println("new obs $observation.id")
+            }
+
+            override fun onError(observation: Observation?, registration: Registration?, error: Exception?) {
+                println("new obs $observation.id")
+            }
+
+        })
+    }
+
+    fun make(registration: Registration, path: String) {
+        println("path $path")
+        val readRequest = ReadRequest(path)
+        println("serv ${this.server}")
+        val readResponse: ReadResponse = this.server.send(registration, readRequest)
+        println("reg $registration")
+        println("serv ${this.server}")
+    }
+
+    fun readAll() {
+        try {
+
+            this.server.registrationStore.allRegistrations.forEach { registration ->
+                run {
+                    println("registration ${registration.id}")
+                    make(registration, "/3303/0/5700")
+                }
+            }
+        } catch (e: Exception) {
+            println("error on readAll ${e.message}")
+        }
+
+    }
+
+    fun initServerRegistrationService() {
+        this.server.registrationService.addListener(object : RegistrationListener {
+            override fun registered(
+                registration: Registration, previousReg: Registration?,
+                previousObsersations: Collection<Observation?>?
+            ) {
+                println("new device: ${registration.id}")
+                server.registrationStore.addRegistration(registration)
+                readAll()
+            }
+
+            override fun updated(update: RegistrationUpdate?, updatedReg: Registration, previousReg: Registration?) {
+                println("device is still here: ${updatedReg.id}")
+                readAll()
+            }
+
+            override fun unregistered(
+                registration: Registration, observations: Collection<Observation?>?, expired: Boolean,
+                newReg: Registration?
+            ) {
+                println("device left: " + registration.endpoint)
+            }
+        })
+    }
 }
 
 fun main(args: Array<String>) {
-	runApplication<Lwm2mServerApplication>(*args)
+    runApplication<Lwm2mServerApplication>(*args)
 }
---
+++ back/src/services/video-storage/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/option"
+)
+
+var driveSvc *drive.Service
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func sendFile(path string) (*drive.File, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	f := &drive.File{Name: "record_" + time.Now().String(), Parents: []string{"1BQZosDDT_J_NtcFfPIOQ_m-CvPMuJDOy"}}
+	return driveSvc.Files.Create(f).Media(file).
+		ProgressUpdater(func(now, size int64) { fmt.Printf("%d, %d\r", now, size) }).
+		Do()
+}
+
+func saveRecord(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	switch r.Method {
+	case "POST":
+		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		f, err := sendFile("/home/pi/ha/config/record")
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		}
+		w.Write([]byte(f.Id))
+	default:
+		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+	}
+}
+
+func main() {
+	ctx := context.Background()
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, drive.DriveMetadataReadonlyScope, drive.DriveFileScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
+	driveSvc, err = drive.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Panicln(err)
+	}
+	http.HandleFunc("/save_record", saveRecord)
+
+	fmt.Printf("Starting server for testing HTTP POST...\n")
+	if err := http.ListenAndServe(":12345", nil); err != nil {
+		log.Fatal(err)
+	}
+}
